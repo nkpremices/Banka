d1f65cb2cb70eeef5675217bc0b668d9c42c2efe
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-31 19:52:55 +0200

    [Feature #164912266] Link the registration page to the home page

--- UI/js/manage.registration.js
+++ UI/js/manage.registration.js
@@ -1,3 +1,11 @@
+// fetching a variable comming from the home page
+const params = location.search.substring(1).split("&");
+const temp = params[0].split("=");
+const hide = temp[1];
+
+// A variable to fetch actions on the registration page
+let registration;
+
 // Functions to shift the sign in and sign up parts
 const signIn = () => {
     //changing the navigation display
@@ -8,6 +16,8 @@ const signIn = () => {
     //shifting to the sign in part
     document.querySelector('.signup-form').className = 'hide';
     document.querySelector('.signin-form').className = 'signin-form';
+
+    registration = true;
 };
 
 const signUp = () => {
@@ -19,7 +29,15 @@ const signUp = () => {
     //shifting to the signup part
     document.querySelector('.form .hide').className = 'signup-form';
     document.querySelector('.signin-form').className = 'signin-form hide';
+
+    registration = true;
 };
+
+// a funtion to return back on the home page
+const backToHome = () => {
+    window.location = '../index.html';
+};
+
 
 // Going to sign in from the form
 document.getElementById('go-to-signin').addEventListener('click', signIn);
@@ -27,3 +45,16 @@ document.getElementById('go-to-signin').addEventListener('click', signIn);
 // Going to sign up from the form
 document.getElementById('go-to-signup').addEventListener('click', signUp);
 
+// Going to the home page
+document.getElementById('home-nav').addEventListener('click', backToHome);
+
+// Going to sign up from the navigation
+document.getElementById('signup-nav').addEventListener('click', signUp);
+
+// Going to sign in from the navigation
+document.getElementById('signin-nav').addEventListener('click', signIn);
+
+// Trynig to see which form will be hide
+if(!registration){
+    if(hide) signIn();
+}
