86ae9b6a9d5d51c5e96ec7c962682d01c4519021
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-06 07:55:17 +0200

    [Chore #165156168] Update the content of the home js management file

--- UI/js/manage.home.js
+++ UI/js/manage.home.js
@@ -1,11 +1,11 @@
 // Going to sign in page from the nav
 
-const callSignin = () => { 
+const callSignin = () => {
     window.location = 'html/signUpIn.html?hide=true';
 };
 
 // Going to sign up page from the nav
-const callSignup  = () => { 
+const callSignup = () => {
     window.location = 'html/signUpIn.html';
 };
 
@@ -17,14 +17,14 @@ const callHome = () => {
     document.getElementById('signup-nav').className = 'li nav-inactive';
 };
 
-//Going to the home page
+// Going to the home page
 document.getElementById('home-nav').addEventListener('click', callHome);
 document.getElementById('logo').addEventListener('click', callHome);
 document.getElementById('app-name').addEventListener('click', callHome);
 
-//Going to the signin page
+// Going to the signin page
 document.getElementById('signin-nav').addEventListener('click', callSignin);
 
-//Going to the signUp page
+// Going to the signUp page
 document.getElementById('signup-nav').addEventListener('click', callSignup);
 document.getElementById('start').addEventListener('click', callSignup);
