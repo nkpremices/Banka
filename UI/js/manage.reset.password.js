39abac7af08205db9eef304ae9e5e7b67771e636
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-06 07:46:50 +0200

    [Chore #165156168] Update the content of the reset password js management file

--- UI/js/manage.reset.password.js
+++ UI/js/manage.reset.password.js
@@ -1,11 +1,11 @@
 // Going to sign in page from the nav
 
-const callSignin = () => { 
+const callSignin = () => {
     window.location = '../html/signUpIn.html?hide=true';
 };
 
 // Going to sign up page from the nav
-const callSignup  = () => { 
+const callSignup = () => {
     window.location = '../html/signUpIn.html';
 };
 
@@ -14,15 +14,15 @@ const backToHome = () => {
     window.location = '../index.html';
 };
 
-//Going to the home page
+// Going to the home page
 document.getElementById('home-nav').addEventListener('click', backToHome);
 document.getElementById('logo').addEventListener('click', backToHome);
 document.getElementById('app-name').addEventListener('click', backToHome);
 
-//Going to the signin page
+// Going to the signin page
 document.getElementById('signin-nav').addEventListener('click', callSignin);
 
-//Going to the signUp page
+// Going to the signUp page
 document.getElementById('signup-nav').addEventListener('click', callSignup);
 
 const admin = () => {
