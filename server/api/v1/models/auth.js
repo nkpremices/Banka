8650473fb6b7ca9b61bfb213ad43bd5f986990d9
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-08 15:47:55 +0200

    [feature #165149953] Update the users model to support the isAdmin and type fields on user creation

--- server/api/v1/models/auth.js
+++ server/api/v1/models/auth.js
@@ -5,14 +5,15 @@ let id = 0;
 
 // a function to save a user when requested
 const saveUser = (email, firstName,
-    // eslint-disable-next-line no-unused-vars
-    lastName, password) => new Promise((resolve, reject) => {
+    lastName, password, type, // eslint-disable-next-line no-unused-vars
+    isAdmin, isLoggedIn) => new Promise((resolve, reject) => {
     // creating a temp user
     const tempUser = new dataStructureDb.schemas.UsersSchema(id += 1,
-        email, firstName, lastName, password);
+        email, firstName, lastName, password, type, isAdmin, isLoggedIn);
 
     // Storing the user
     dataStructureDb.storages.usersStorage.push(tempUser);
+    // console.log(dataStructureDb.storages.usersStorage);
     resolve(tempUser);
 });
 
