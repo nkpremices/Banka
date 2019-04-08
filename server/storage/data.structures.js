5d3a76e40068df445e618d79418c14fb270b3fb1
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-08 15:48:43 +0200

    [feature #165149953] Give isAdmin and type attributes to the users schema

--- server/storage/data.structures.js
+++ server/storage/data.structures.js
@@ -6,12 +6,15 @@
 class UsersSchema {
     constructor(id,
         email, firstName, lastName,
-        password) {
+        password, type, isAdmin, isLoggedIn) {
         this.id = id;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.password = password;
+        this.type = type;
+        this.isAdmin = isAdmin;
+        this.isLoggedIn = isLoggedIn;
     }
 }
 
