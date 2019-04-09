4d2dbf8d46931eba8e12545e0ceac665bfb43ba2
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-09 22:41:32 +0200

    [Feature #165150128] Give the new account testing file

---
+++ server/tests/v1/accounts.js
@@ -0,0 +1,69 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../index';
+import environment from '../../configs/environnements';// eslint-disable-line
+
+const should = chai.should();// eslint-disable-line
+
+chai.use(chaiHttp);
+
+const accountCreationTemp = {
+    accountName: 'scolarship',
+    currency: 'usd',
+    type: 'curent',
+    status: 'active',
+};
+
+describe('Accounts', () => {// eslint-disable-line
+    describe('POST - /api/v1/accounts', () => {// eslint-disable-line
+        let userToken;
+        before((done) => {// eslint-disable-line
+            const user = {
+                email: 'premices@gmail.com',
+                firstName: 'premices',
+                lastName: 'tuverer',
+                password: 'dddddd4U',
+                type: 'client',
+                isAdmin: 'true',
+            };
+            chai
+                .request(app)
+                .post('/api/v1/auth/signup')
+                .send(user)
+                // eslint-disable-next-line no-unused-vars
+                .end((err, res) => {
+                    done();
+                });
+        });
+
+        it('it should login test user', (done) => {// eslint-disable-line
+            // login and get test user token
+            const user1 = {
+                email: 'premices@gmail.com',
+                password: 'dddddd4U',
+            };
+
+            chai
+                .request(app)
+                .post('/api/v1/auth/signin')
+                .send(user1)
+                .end((err, res) => {
+                    userToken = res.body.data.token;
+                    done();
+                });
+        });
+
+        it('it should create an account', (done) => {// eslint-disable-line
+            chai
+                .request(app)
+                .post('/api/v1/accounts')
+                .set('token', `${userToken}`)
+                .send(accountCreationTemp)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.data.should.be.a('object');
+                    done();
+                });
+        });
+    });
+});
