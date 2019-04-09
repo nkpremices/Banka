eac460cee94f624beaaf74d2979a1e0a012e94a3
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-09 15:10:42 +0200

    [Feature #165150128] Give the accounts route to the server main router

--- server/api/v1/routes/index.js
+++ server/api/v1/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import authRouter from './auth';
+import accountsRouter from './accounts';
 
 const v1 = express();
 
 
 v1.use('/auth', authRouter);
+v1.use('/accounts', accountsRouter);
 
 export default v1;
