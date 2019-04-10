bfea7c798987de2db3912d11fc8824342994cd2b
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-10 16:49:05 +0200

    [Feature #165150148] Update the content of the account verification file

--- server/helpers/v1/account.verification.js
+++ server/helpers/v1/account.verification.js
@@ -1,6 +1,13 @@
 import dataStructureDb from '../../storage/data.structures';
 
-const accountVerification = name => dataStructureDb
-    .storages.accountsStorage.find(el => el.accountName === name);
+const accountVerification = {
+    name: (name, owner) => dataStructureDb.storages
+        .accountsStorage
+        .find(el => el.accountName === name && el.owner === owner),
+    number: number => dataStructureDb.storages
+        .accountsStorage
+        .find(el => el.accountNumber === number),
+
+};
 
 export default accountVerification;
