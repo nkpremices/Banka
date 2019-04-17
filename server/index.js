5b4acd053c587c6ea6cc896362a45f426877392a
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-17 22:17:30 +0200

    [Feature #165407651] Add routes for v2 on the entry point

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ import '@babel/polyfill';
 import express from 'express';
 import dotenv from 'dotenv';
 import v1 from './api/v1/routes/index';
-import docs from './api/v1/routes/docs';
+import v1Docs from './api/v1/routes/docs';
+import v2 from './api/v2/routes/index';
+import v2Docs from './api/v2/routes/docs';
 import home from './api/v1/routes/home';
 import environement from './configs/environnements';
 import registerMiddleware from './middlewares/register';
@@ -18,11 +20,18 @@ registerMiddleware(app);
 // Creating the admin v1
 createAdmin();
 
-// Initializing the db
-initializeDb();
+// Initializing the db v2
+initializeDb.createTables();
 
+
+// App for v1
 app.use('/api/v1', v1);
-app.use('/docs/v1', docs);
+app.use('/docs/v1', v1Docs);
+
+// App for v2
+app.use('/api/v2', v2);
+app.use('/docs/v2', v2Docs);
+
 app.use('/', home);
 
 app.listen(`${environement.app.port}`, () => {
