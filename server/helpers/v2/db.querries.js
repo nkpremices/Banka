6e5b4c18a53ff42dc36da282a3063bcd352b2ace
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-17 22:21:01 +0200

    [Feature #165407651] Add a querry to find users by id and email

--- server/helpers/v2/db.querries.js
+++ server/helpers/v2/db.querries.js
@@ -5,10 +5,15 @@ const insertUser = `INSERT INTO users (email, firstName, lastName,
     password, type, isAdmin, isLogedIn)
     VALUES($1, $2, $3, $4, $5, $6, $7) returning *`;
 const findUserByEmail = email => `SELECT * FROM users WHERE email = '${email}'`;
+const findUserByEmailAndId = (id, email) => `SELECT * FROM users WHERE 
+    (id = '${id}' AND email = '${email}')`;
+const dropTables = 'DROP TABLE IF EXISTS users CASCADE;';
 
 const queries = {
     insertUser,
     findUserByEmail,
+    findUserByEmailAndId,
+    dropTables,
 };
 
 export default queries;
