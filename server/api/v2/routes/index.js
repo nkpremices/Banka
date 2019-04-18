a002d8113f81f51005b6440af9ab650c636bd247
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-18 22:35:43 +0200

    [Feature #165438986] Give to the server a v2 main router an accounts route

--- server/api/v2/routes/index.js
+++ server/api/v2/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRouter from './auth';
+import accountsRouter from './accounts';
 
 
 const v2 = express();
 
 
 v2.use('/auth', authRouter);
+v2.use('/accounts', accountsRouter);
 
 export default v2;
