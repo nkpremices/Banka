4333771a8afcf9295dede24031b188ab966b2a74
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-18 07:37:57 +0200

    [Feature #165437071] Add querries on the db.querries helper

--- server/helpers/v2/db.querries.js
+++ server/helpers/v2/db.querries.js
@@ -1,19 +1,22 @@
 /**
  * Here will be all the needed queries */
 
-const insertUser = `INSERT INTO users (email, firstName, lastName,
-    password, type, isAdmin, isLogedIn)
+const insertUser = `INSERT INTO users (email, firstname, lastname,
+    password, type, isadmin, isloggedin)
     VALUES($1, $2, $3, $4, $5, $6, $7) returning *`;
 const findUserByEmail = email => `SELECT * FROM users WHERE email = '${email}'`;
 const findUserByEmailAndId = (id, email) => `SELECT * FROM users WHERE 
     (id = '${id}' AND email = '${email}')`;
 const dropTables = 'DROP TABLE IF EXISTS users CASCADE;';
+const setUserLogedIn = id => `UPDATE users SET 
+    isloggedin = true WHERE id = ${id};`;
 
 const queries = {
     insertUser,
     findUserByEmail,
     findUserByEmailAndId,
     dropTables,
+    setUserLogedIn,
 };
 
 export default queries;
