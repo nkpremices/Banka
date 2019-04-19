af5ddb89696077b7b4986e7bf70fa18e793c0621
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-19 23:13:53 +0200

    [Feature #165471510] Give the transactions table to the db file

--- server/storage/db.js
+++ server/storage/db.js
@@ -2,10 +2,10 @@ const usersTable = `CREATE TABLE IF NOT EXISTS
 users(
   id SERIAL PRIMARY KEY,
   email VARCHAR(128) UNIQUE NOT NULL,
-  firstname VARCHAR(128) NULL,
-  lastname VARCHAR(128) NULL,  
+  firstname VARCHAR(128) NOT NULL,
+  lastname VARCHAR(128) NOT NULL,  
   password VARCHAR(128) NOT NULL,
-  type VARCHAR(128) NULL,
+  type VARCHAR(128) NOT NULL,
   isadmin BOOLEAN DEFAULT FALSE,
   isloggedin BOOLEAN DEFAULT FALSE
 );`;
@@ -13,7 +13,7 @@ const accountsTable = `CREATE TABLE IF NOT EXISTS
 accounts(
   id SERIAL PRIMARY KEY,
   accountname VARCHAR(128) UNIQUE NOT NULL,
-  accountnumber FLOAT NOT NULL,
+  accountnumber FLOAT UNIQUE NOT NULL,
   createdon FLOAT NOT NULL,  
   owner INTEGER REFERENCES users(id) NOT NULL,
   type VARCHAR(128) NOT NULL,
@@ -22,9 +22,22 @@ accounts(
   balance FLOAT NOT NULL
 );`;
 
+const transactionsTable = `CREATE TABLE IF NOT EXISTS
+transactions(
+  id SERIAL PRIMARY KEY,
+  createdon FLOAT NOT NULL,
+  type VARCHAR(128) NOT NULL,
+  accountnumber FLOAT NOT NULL,
+  cashier INTEGER REFERENCES users(id) NOT NULL,
+  amount FLOAT NOT NULL,
+  oldbalance FLOAT NOT NULL,
+  newbalance FLOAT NOT NULL
+);`;
+
 const tables = {
     usersTable,
     accountsTable,
+    transactionsTable,
 };
 
 export default tables;
