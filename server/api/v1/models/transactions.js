b6f9885b3ce5edd9c610206c2362da85f0fa28b9
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-21 12:57:09 +0200

    [Chore #165483858] Improve the code of the v1 transactions model

--- server/api/v1/models/transactions.js
+++ server/api/v1/models/transactions.js
@@ -4,22 +4,24 @@ let id = 0;
 
 // a function to save a transaction when requested
 const saveTransaction = {
-    // eslint-disable-next-line no-unused-vars
     credit: (tempAccount, amount, cashier) => new Promise((resolve, reject) => {
         // get the old balance of the account
         const oldBalance = tempAccount.balance;
         // Credit the account
         const newBalance = tempAccount.credit(amount);
         // creating a temp transaction
-        const tempTransaction = new dataStructureDb.schemas
-            .TransactionsSchema(id += 1, Date.now(), 'credit',
-                tempAccount.accountNumber, cashier.id, amount,
-                oldBalance, newBalance);
-        // Storing the transaction
-        dataStructureDb.storages.transactionsStorage.push(tempTransaction);
-        resolve(tempTransaction);
+        try {
+            const tempTransaction = new dataStructureDb.schemas
+                .TransactionsSchema(id += 1, Date.now(), 'credit',
+                    tempAccount.accountNumber, cashier.id, amount,
+                    oldBalance, newBalance);
+            // Storing the transaction
+            dataStructureDb.storages.transactionsStorage.push(tempTransaction);
+            resolve(tempTransaction);
+        } catch (error) {
+            reject(new Error('Error when trying to save the transaction'));
+        }
     }),
-    // eslint-disable-next-line no-unused-vars
     debit: (tempAccount, amount, cashier) => new Promise((resolve, reject) => {
         // get the old balance of the account
         const oldBalance = tempAccount.balance;
@@ -28,7 +30,7 @@ const saveTransaction = {
             const errorMsg = 'Insufficient balance for this transaction';
             reject(new Error(errorMsg));
         }
-        // Credit the account
+        // Debit the account
         const newBalance = tempAccount.debit(amount);
         // creating a temp transaction
         const tempTransaction = new dataStructureDb.schemas
