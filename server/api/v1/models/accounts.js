9997dc5c668712ac5ba4ebe16ee2bba7aafcb9ab
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-26 14:24:56 +0200

    [Chore #165592378] Document the v1 accounts model and add a fiew validations on it

--- server/api/v1/models/accounts.js
+++ server/api/v1/models/accounts.js
@@ -1,10 +1,26 @@
+/**
+ * The v1 accounts model file
+ * Conataining all the functions interacting with the accounts
+ * table
+ * @name acountsModelV1
+ */
+
 import dataStructureDb from '../../../storage/data.structures';
 import generateRandomNumber from '../../../helpers/generate.random.number';
 
 
 let id = 0;
 
-// a function to save an account when requested
+/**
+ * A function to save an account when requested by the controller
+ * @param {String} accountName - the name of the account
+ * @param {String} currency - the account currency
+ * @param {String} type - the account type
+ * @param {String} status - the account status
+ * @param {Object} user - an object containing all information
+ * @returns {Promise}
+ *  about a user
+ */
 const saveAccount = (accountName, currency,
     type, status, user) => new Promise((resolve, reject) => {
     try {
@@ -22,19 +38,44 @@ const saveAccount = (accountName, currency,
     }
 });
 
-// A function to verify if an account exists
+/**
+ * An object to contain fuctions to call to get a record from
+ * the database to verify it exists
+ * @property {function} name - A function to get an account from
+ * the database by the account name
+ * @property {function} number - A function to get an account
+ * from the database by acount number
+ * @returns {Promise}
+ */
+
 const verifyAccount = {
+    /**
+     * A function to get an account from the database
+     * by the account name
+     * @param {String} name - The account name of the account
+     * @param {Number} owner - The Id of the owner
+     */
     name: (name, owner) => dataStructureDb.storages
         .accountsStorage
         .find(el => el.accountName === name && el.owner === owner),
+    /**
+     * A function to get an account from the database
+     * by acount number
+     * @param {Number} number - The account number of
+     * the account
+     */
     number: number => dataStructureDb.storages
         .accountsStorage
         .find(el => el.accountNumber === number),
 
 };
 
-//  a function to activate/deactivate an account when requested
-// eslint-disable-next-line no-unused-vars
+/**
+ * A function to call to find an account for doing further
+ * updates on it
+ * @param {Number} accountNumber
+ * @returns {Promise}
+ */
 const findAccount = accountNumber => new Promise((resolve, reject) => {
     // creating a temp account
     const tempAccount = verifyAccount.number(accountNumber);
@@ -46,12 +87,25 @@ const findAccount = accountNumber => new Promise((resolve, reject) => {
     }
 });
 
-// A function to change the satus of an account
+/**
+ * A function to change the satus of an account
+ * @param {Object} account - the account whose status
+ * is going to be changed
+ * @param {String} status - The new status
+ */
 const changeAccountStatus = (account, status) => {
     account.setStatus(status);
 };
 
-// A function to verify the status of an account
+
+/**
+ * A function to verify the status of an account
+ *
+ * @param {Object} account - The account whose status is going
+ * to be verified
+ * @param {string} status - The status to check
+ * @returns {boolean} retObj - True if the status is the same
+ */
 const verifyAccountStatus = (account, status) => {
     const storage = dataStructureDb.storages.accountsStorage;
     let retObj = false;
@@ -63,7 +117,13 @@ const verifyAccountStatus = (account, status) => {
     return retObj;
 };
 
-//  a function to delete an account when requested
+
+/**
+ * a function to delete an account when requested by
+ * the controller
+ * @param {Number} accountNumber - the account number
+ * @returns {Promise}
+ */
 const deleteAccount = accountNumber => new Promise((resolve, reject) => {
     const storage = dataStructureDb.storages.accountsStorage;
 
