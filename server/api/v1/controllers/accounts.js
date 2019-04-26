a46f9afb0da4ceae1833dc84061c6402a6e27681
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-26 14:23:15 +0200

    [Chore #165592378] Document the v1 accounts controller and add a fiew validations on it

--- server/api/v1/controllers/accounts.js
+++ server/api/v1/controllers/accounts.js
@@ -1,11 +1,30 @@
+/**
+ * The v1 accounts controller file
+ * @name accountsControllerV1
+ */
+
 import accountsModel from '../models/accounts';
 import usersModel from '../models/auth';
 import sendError from '../../../helpers/send.error';
+import checkNumber from '../../../helpers/check.number';
+
+/**
+ * An object to contain all the controllers functions
+ * @property {function} createAccount - The controller used
+ * for creating bank accounts
+ * @property {function} activateDeactivateAccount - The
+ * controller used for updating an account's status
+ * @property {function} deleteAccount - The
+ * controller used for deleting an account's status
+ */
 
 
 export default {
 /**
  * POST - / Create a new bank account
+ * @param {object} req - the request object
+ * @param {object} res - the result object
+ * @returns {Promise}
  */
     createAccount: async (req, res) => {
         // Initializing variables
@@ -65,10 +84,13 @@ export default {
             sendError(403, result, res, error);
         }
     },
-    activateDeactivateAccount: async (req, res) => {
     /**
         * PATCH - /<account-number> Create a new bank account
+        * @param {object} req - the request object
+        * @param {object} res - the result object
+        * @returns {Promise}
     */
+    activateDeactivateAccount: async (req, res) => {
         // Initializing variables
         const result = {};
         const resStatus = 200;
@@ -76,43 +98,54 @@ export default {
 
         // getting the body and the account number
         const { status } = req.body;
-        const accountNumber = parseInt(req.params.accountNumber, 10);
+        let { accountNumber } = req.params;
+
+        // Validate the accountNumber
+        accountNumber = checkNumber(req.params
+            .accountNumber) ? parseInt(accountNumber, 10) : false;
         // Getting the token from the header
         // Verifying the token
         const tempUser = usersModel.verifyToken(req.headers.token);
         if (tempUser) {
             if ((tempUser.isAdmin || tempUser.type === 'staff')
             && tempUser.isLoggedIn) {
-                // trying to save the status
-                try {
-                    // See first if the account exists
-                    const tempAccount = await accountsModel
-                        .findAccount(accountNumber);
+                if (accountNumber) {
+                    // trying to save the status
+                    try {
+                        // See first if the account exists
+                        const tempAccount = await accountsModel
+                            .findAccount(accountNumber);
 
-                    // Veryfying the status first
-                    const verify = accountsModel
-                        .verifyAccountStatus(tempAccount, status);
+                        // Veryfying the status first
+                        const verify = accountsModel
+                            .verifyAccountStatus(tempAccount, status);
 
-                    // Display a custom message if the
-                    // status is the same
-                    if (verify) {
-                        error = 'The provided status is the '
-                        + 'same as the current';
-                        sendError(205, result, res, error);
-                    } else {
-                        // changing the account status
-                        accountsModel.changeAccountStatus(tempAccount, status);
-                        // Sending back the required object
-                        result.status = resStatus;
-                        result.message = 'Account updated successfully';
-                        result.data = {
-                            accountNumber: tempAccount.accountNumber,
-                            status: tempAccount.status,
-                        };
-                        res.status(resStatus).json(result);
+                        // Display a custom message if the
+                        // status is the same
+                        if (verify) {
+                            error = `Account ${tempAccount.accountNumber} `
+                            + `is already ${status}`;
+                            sendError(205, result, res, error);
+                        } else {
+                            // changing the account status
+                            accountsModel
+                                .changeAccountStatus(tempAccount, status);
+                            // Sending back the required object
+                            result.status = resStatus;
+                            result.message = 'Account updated successfully';
+                            result.data = {
+                                accountNumber: tempAccount.accountNumber,
+                                status: tempAccount.status,
+                            };
+                            res.status(resStatus).json(result);
+                        }
+                    } catch (err) {
+                        sendError(404, result, res, `${err}`
+                            .replace('Error', ''));
                     }
-                } catch (err) {
-                    sendError(404, result, res, `${err}`.replace('Error', ''));
+                } else {
+                    error = 'Invalid account number provided';
+                    sendError(400, result, res, error);
                 }
             } else {
                 error = 'Only a logged in admin/staff can activate/deactivate '
@@ -124,17 +157,24 @@ export default {
             sendError(403, result, res, error);
         }
     },
+    /**
+        * DELETE- /<account-number> Delete a bank account
+        * @param {object} req - the request object
+        * @param {object} res - the result object
+        * @returns {Promise}
+    */
     deleteAccount: async (req, res) => {
-        /**
-            * DELETE- /<account-number> Delete a bank account
-        */
         // Initializing variables
         const result = {};
         const resStatus = 200;
         let error;
 
         // getting the the account id
-        const accountNumber = parseInt(req.params.accountNumber, 10);
+        let { accountNumber } = req.params;
+
+        // Validate the accountNumber
+        accountNumber = checkNumber(req.params
+            .accountNumber) ? parseInt(accountNumber, 10) : false;
 
         // Getting the token from the header
         // Verifying the token
@@ -142,24 +182,30 @@ export default {
         if (tempUser) {
             if ((tempUser.isAdmin || tempUser.type === 'staff')
                 && tempUser.isLoggedIn) {
-                // trying to delete the account
-                try {
+                if (accountNumber) {
+                    // trying to delete the account
+                    try {
                     // Trying to see if the account exists
-                    const tempAccount = await accountsModel
-                        .findAccount(accountNumber);
+                        const tempAccount = await accountsModel
+                            .findAccount(accountNumber);
 
-                    // deleting the account
-                    await accountsModel
-                        .deleteAccount(tempAccount.accountNumber);
+                        // deleting the account
+                        await accountsModel
+                            .deleteAccount(tempAccount.accountNumber);
 
-                    // Sending back the required object
-                    result.status = resStatus;
-                    result.data = {
-                        message: 'Account successfully deleted',
-                    };
-                    res.status(resStatus).json(result);
-                } catch (err) {
-                    sendError(404, result, res, `${err}`.replace('Error', ''));
+                        // Sending back the required object
+                        result.status = resStatus;
+                        result.data = {
+                            message: 'Account successfully deleted',
+                        };
+                        res.status(resStatus).json(result);
+                    } catch (err) {
+                        sendError(404, result, res, `${err}`
+                            .replace('Error', ''));
+                    }
+                } else {
+                    error = 'Invalid account number provided';
+                    sendError(400, result, res, error);
                 }
             } else {
                 error = 'Only a logged in admin/staf can delete '
