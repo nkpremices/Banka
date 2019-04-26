e741579ee1bc8658235a295b9c8e668abd74a3b1
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-26 14:31:11 +0200

    [Chore #165592378] Document the hekper to compare two passwords

--- server/helpers/compare.passwords.js
+++ server/helpers/compare.passwords.js
@@ -1,5 +1,15 @@
 import bcrypt from 'bcrypt';
 
+/**
+ * A fucnction to compare if two passwords match.
+ * One of them is hashed
+ *
+ * @param {Object} tempUser - The user whose password
+ * will be checked
+ * @param {string} password - The provided password from
+ * the client
+ * @returns {Promise}
+ */
 const comparePasswords = (tempUser,
     password) => new Promise((resolve, reject) => {
     try {
