a804f4f46f3eaef0aed857fe7c8c0faff9ce1083
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-26 14:32:06 +0200

    [Chore #165592378] Document the hekper to create random numbers

--- server/helpers/generate.random.number.js
+++ server/helpers/generate.random.number.js
@@ -1,5 +1,9 @@
 import environment from '../configs/environnements';
 
+/**
+ * A fucntion to gnerate random numbers
+ *
+ */
 const generateRandomNumber = () => Math
     .floor(Math.random() * (10 ** environment.accountNumberLength));
 
