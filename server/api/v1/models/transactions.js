d05edaa50310188dc62501196076775121029d6e
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-26 14:25:29 +0200

    [Chore #165592378] Document the v1 transaction model and add a fiew validations on it

--- server/api/v1/models/transactions.js
+++ server/api/v1/models/transactions.js
@@ -1,9 +1,30 @@
+/**
+ * The v1 transactions model file
+ * @name transactionsModelV1
+ */
+
 import dataStructureDb from '../../../storage/data.structures';
 
 let id = 0;
 
-// a function to save a transaction when requested
+/**
+ * An obect to contain methods to save a
+ * transaction wether it's credit or debit
+ * @property {function} credit - The function
+ * to credit an account
+ * @property {function} debit - The function
+ * to debit an account
+ */
 const saveTransaction = {
+    /**
+     * A fuction to credit an account
+     *
+     * @param {Object} tempAccount - The account to credit
+     * @param {Number} amount - The amount of money to credit
+     * @param {Number} cashier - The id of the cashier who
+     * executes the transaction
+     * @returns {Promise}
+     */
     credit: (tempAccount, amount, cashier) => new Promise((resolve, reject) => {
         // get the old balance of the account
         const oldBalance = tempAccount.balance;
@@ -22,6 +43,15 @@ const saveTransaction = {
             reject(new Error('Error when trying to save the transaction'));
         }
     }),
+    /**
+     * A fuction to debit an account
+     *
+     * @param {Object} tempAccount - The account to debit
+     * @param {Number} amount - The amount of money to debit
+     * @param {Number} cashier - The id of the cashier who
+     * executes the transaction
+     * @returns {Promise}
+     */
     debit: (tempAccount, amount, cashier) => new Promise((resolve, reject) => {
         // get the old balance of the account
         const oldBalance = tempAccount.balance;
