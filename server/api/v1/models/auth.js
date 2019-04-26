7d304ff0c3751565f0fc992370f47961b2bf71c8
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-26 14:22:00 +0200

    [Chore #165592378] Document the v1 auth model and add a fiew validations on it

--- server/api/v1/models/auth.js
+++ server/api/v1/models/auth.js
@@ -1,3 +1,9 @@
+/**
+ * The v1 users model file
+ * Conataining all the functions interacting with the users table
+ * @name usersModelV1
+ */
+
 import dataStructureDb from '../../../storage/data.structures';
 import comparePasswords from '../../../helpers/compare.passwords';
 import decodeJwt from '../../../helpers/decode.token';
@@ -5,7 +11,18 @@ import decodeJwt from '../../../helpers/decode.token';
 
 let id = 0;
 
-// a function to save a user when requested
+/**
+ * A function to save a user when requested
+ *
+ * @param {String} email - The email of the user
+ * @param {String} firstName - The first name of the user
+ * @param {String} lastName - The last name of the user
+ * @param {String} password - The password of the user
+ * @param {String} type - The type of the user
+ * @param {boolean} isAdmin - True if it's an admin user
+ * @param {boolean} isLoggedIn - True if the user is logged in
+ * @returns {Promise}
+ */
 const saveUser = (email, firstName, lastName, password, type,
     isAdmin, isLoggedIn) => new Promise((resolve, reject) => {
     try {
@@ -22,7 +39,15 @@ const saveUser = (email, firstName, lastName, password, type,
     }
 });
 
-// A fuction to validate the decoded data
+
+/**
+ * A function to validate a token. It decodes it
+ * then see if there is a user that matches in the database
+ *
+ * @param {String} token - The provided token
+ * @returns {Object} tempUsr - The object containing the users
+ * informations
+ */
 const verifyToken = (token) => {
     const userInfo = decodeJwt(token);
     let tempUser;
@@ -34,8 +59,16 @@ const verifyToken = (token) => {
     return tempUser;
 };
 
-// A function to verify if a stored user is an admin
-// on admin/staff account creation
+/**
+ * A function to verify if a stored user is an admin
+ * on admin/staff account creation
+ * @param {String} email - The provided email from the client
+ * @param {String} type - The provided type from the client
+ * @param {Boolean} isAdmin - True if it's an admin
+ * @param {String} AdminToken - The  token of the user
+ * @returns {object} retObj - An object to map the
+ * verification process
+ */
 const VerifiUser = (email, type, isAdmin, AdminToken) => {
     const tempUser = dataStructureDb
         .storages.usersStorage
@@ -66,8 +99,13 @@ const VerifiUser = (email, type, isAdmin, AdminToken) => {
     return retObj;
 };
 
-// A function to find if a user is stored
-// eslint-disable-next-line no-unused-vars
+/**
+ * A function to check users credentials on login
+ *
+ * @param {String} email - The providded email
+ * @param {String} password - The provided password
+ * @returns {Promise}
+ */
 const findUser = (email, password) => new Promise(async (resolve, reject) => {
     // An object to return
     const retObj = {
@@ -92,7 +130,12 @@ const findUser = (email, password) => new Promise(async (resolve, reject) => {
     resolve(retObj);
 });
 
-// A function to login a user
+
+/**
+ * A function to login a user
+ *
+ * @param {Object} user - The user to login
+ */
 const loginUser = (user) => {
     user.login();
 };
