ac1abe36db333accc833ef10f2a5a38917f22f1e
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-04-26 14:33:32 +0200

    [Chore #165592378] Document the middleware to register the app

--- server/middlewares/register.js
+++ server/middlewares/register.js
@@ -1,8 +1,16 @@
+/**
+ * Resgister midllemware file
+ * @name register
+ */
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import logger from 'morgan';
 import environment from '../configs/environnements';
-
+/**
+ * A function to register all the needed middlewares to the
+ * app (express instance) every time the server is starting
+ * @param {object} app - The express instance
+ */
 export default (app) => {
     app
     // Parse req object and make data available on req.body
